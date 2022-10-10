af4a9ac16b730f22f5c7a13cb7559274f86c2035
imvrba <imvrba@iastate.edu>
2022-10-09 22:49:22 -0500

    pushing and then making issues

--- gubs_game.cpp
+++ gubs_game.cpp
@@ -1,1 +1,25 @@
+//
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TOTAL_NUM_CARDS 70;
+
+//define functions here
+    //shuffle remaining cards
+
+    //look at the top three cards
+
+//define global variables
+int remainingCards;
+
+
+//main
+int main() {
+    //initial remarks
+    printf("Starting a fresh game of Gubs");
+
+
+
+} //end of main
