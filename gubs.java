238cd919e4607a6b5f884ace77ea1aa9733e9c7a
imvrba <imvrba@iastate.edu>
2022-10-15 01:25:46 -0500

    made the starting cards

--- gubs.java
+++ gubs.java
@@ -1,63 +1,45 @@
 /*
- * 
+ * start with 13 gubs and 4 spears
  */
+import java.io.*;
+import java.math.*; 
+import java.util.Random;
+
 
 public class gubs {
+
 	public static void main(String[] args) {
-		printOne();
-		printOne();
-		printTwo();
+		System.out.println("Starting a fresh game of gubs");
+		//assign card values to card ids
+		//shuffle remaining cards
+		int numberOfCards = 17;
+		Random rand = new Random();
+
+		int deck[] = new int[numberOfCards]; //13 gubs, 4 spears
+
+		for (int i = 0; i < numberOfCards; i++)	{
+			deck[i] = i;	//0 can be esteemed?
+			//System.out.println();
+		}
+
+
+		//shuffle and print cards
+		for(int cardID:deck)	{
+			//int source = rand.nextInt() % 52;
+			//int dest = rand.nextInt() % 52;
+
+			//System.out.println(source);
+			//System.out.println(dest);
+
+			// int shuffleCard = deck[source];
+	        // deck[source] = deck[dest];
+	        // deck[dest] = shuffleCard;
+			
+			System.out.println(cardID);
+		}
+		
+		
 	}
 
-	public static void printOne() {
-		System.out.println("Hello World");
-	}
 
-	public static void printTwo() {
-		printOne();
-		printOne();
-	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// int main() {
-//     //initial remarks
-//     printf("Starting a fresh game of Gubs...\n");
-
-//     int deck[70];
-
-//     //initialize the card deck
-//     for (int i=0; i < 70; i++) {
-//         deck[i] = i;
-//     }
-
-//     //shuffle cards
-//     for (int i=0; i < 1000; i++)   {
-//         int source = rand() % 52;
-//         int dest = rand() % 52;
-//         int shuffleCard = deck[source];
-//         deck[source] = deck[dest];
-//         deck[dest] = shuffleCard;
-//     }
-
-//     //show shuffled cards
-//     if (DEV_MODE == 1) {
-//         printf("List of Shuffled Cards:\n");
-//         for(int i = 0; i < 70; i++){
-//             printf("%d\n", deck[i]);
-//         }
-//     }
-
-// } //end of main
