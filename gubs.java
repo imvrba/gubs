c43de035a963d325b8023e4616f9fdcade355fbc
imvrba <imvrba@iastate.edu>
2022-10-23 01:54:35 -0500

    adding cards

--- gubs.java
+++ gubs.java
@@ -5,15 +5,15 @@ import java.util.Random;
 
 
 public class gubs {
-	//int remainingCards = 17; //make this an array
+	int remainingCards = 17; //make this an array
 
 
 	public static void shuffleRemainingCards(int deck[]) {
 		Random rand = new Random();
 
 		for (int first = 0; first < deck.length; first++)	{
-			//select a random number between 0 and 17
-			int second  = rand.nextInt(17);
+			//select a random number between 0 and 70
+			int second  = rand.nextInt(70);
 
 			//swap cards
 			int temp = deck[first];
@@ -21,6 +21,23 @@ public class gubs {
 			deck[second] = temp;
 		}
 		return;
+	}
+
+	//used at the begining of the game
+	static void defineCards(int deck[])	{
+		//define Gubs (Q:13)
+		for(int i = 0; i < 12; i++)	{
+			deck[i].name = "Gub";
+		}
+
+		for(i = 13; i < 70; i++)	{
+			deck[i].name = "Mushroom";
+		}
+	}
+
+
+
+	public gubs() {
 	}
 
 
@@ -34,7 +51,7 @@ public class gubs {
 		int numberOfCards = 70;
 		Random rand = new Random();
 
-		int deck[] = new int[numberOfCards]; //13 gubs, 4 spears
+		int deck[] = new int[numberOfCards];
 
 		for (int i = 0; i < numberOfCards; i++)	{
 			deck[i] = i;	//0 can be esteemed?
@@ -45,7 +62,6 @@ public class gubs {
 
 		//shuffle and print cards
 		for(int cardID:deck)	{
-
 			System.out.println(cardID);
 		}
 		
